5426274f71762e33b30944b721edf8f8bf987edd
ZHJ <1803372589@qq.com>
2019-07-01 16:33:57 +0800

    这是第一次提交

---
+++ app.js
@@ -0,0 +1,22 @@
+// 引入express
+const express = require("express");
+// 引入fs模块
+const fs = require("fs");
+// 创建应用
+const app = express();
+// 开启服务器，并监听端口号
+app.listen(3000, () => {
+  console.log("http://127.0.0.1:3000");
+});
+
+// 添加静态资源托管
+app.use("/assets", express.static("assets"));
+app.use("/uploads", express.static("uploads"));
+
+// 添加路由配置
+app.get("/", (req, res) => {
+  fs.readFile(__dirname + "/views/admin/index.html", (err, data) => {
+    if (err) res.end("404");
+    res.end(data);
+  });
+});
